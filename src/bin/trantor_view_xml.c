9cfa79369eed95d2eff2cb71aea12021befad2c6
Jorge Zapata <jorgeluis.zapata@gmail.com>
2013-06-12 23:31:50 +0000

    Add the code to handle the attributes, once the tspan is implement it should be enabled

--- src/bin/trantor_view_xml.c
+++ src/bin/trantor_view_xml.c
@@ -9,102 +9,6 @@ typedef struct _Trantor_View_Xml
 
 static Eina_Bool _generate_node(Egueb_Dom_Node *n, void *data);
 
-#if 0
-static char * _myelement_to_string(Egueb_Dom_Node *n)
-{
-	MyElement *thiz;
-	Egueb_Dom_Node *doc = NULL;
-	Egueb_Dom_String *name;
-	Egueb_Dom_Node_Map_Named *attrs = NULL;
-	char *str = NULL;
-	char *ret = NULL;
-
-	thiz = MYELEMENT(n);
-	egueb_dom_element_tag_name_get(n, &name);
-	str = eina_str_dup_printf("<%s", egueb_dom_string_string_get(name));
-	/* dump every proeprty */
-	egueb_dom_node_attributes_get(n, &attrs);
-	if (attrs)
-	{
-		int count;
-		int i;
-
-		count = egueb_dom_node_map_named_length(attrs);
-		for (i = 0; i < count; i++)
-		{
-			Egueb_Dom_Node *attr = NULL;
-			Egueb_Dom_String *attr_name;
-			Egueb_Dom_String *attr_value = NULL;
-
-			egueb_dom_node_map_named_at(attrs, i, &attr);
-			if (!attr) continue;
-			if (!egueb_dom_attr_is_set(attr)) goto no_name;
-
-			egueb_dom_attr_name_get(attr, &attr_name);
-			if (!attr_name) goto no_name;
-
-			egueb_dom_attr_string_get(attr, &attr_value);
-			if (!attr_value) goto no_value;
-
-			ret = eina_str_dup_printf("%s %s=\"%s\"", str,
-					egueb_dom_string_string_get(attr_name),
-					egueb_dom_string_string_get(attr_value));
-			free(str);
-			str = ret;
-
-			egueb_dom_string_unref(attr_value);
-no_value:
-			egueb_dom_string_unref(attr_name);
-no_name:
-			egueb_dom_node_unref(attr);
-		}
-		egueb_dom_node_map_named_free(attrs);
-	}
-	/* nor some meta information */
-	egueb_dom_node_document_get(n, &doc);
-	ret = eina_str_dup_printf("%s> (ref: %d, doc: %p)", str,
-			egueb_dom_node_ref_get(n), doc);
-	free(str);
-	egueb_dom_string_unref(name);
-	if (doc) egueb_dom_node_unref(doc);
-
-	return ret;
-}
-
-static void _myelement_dump(Egueb_Dom_Node *thiz, Eina_Bool deep, int level)
-{
-	Egueb_Dom_Node *child = NULL;
-	char *str;
-	int i;
-
-	for (i = 0; i < level; i++)
-	{
-		printf(" ");
-	}
-	/* print the element */
-	str = _myelement_to_string(thiz);
-	printf("%s\n", str);
-	free(str);
-
-	/* in case of deep, also do the children */
-	if (!deep) goto done;
-
-	egueb_dom_node_child_first_get(thiz, &child);
-	if (!child) goto done;
-
-	do
-	{
-		Egueb_Dom_Node *sibling = NULL;
-
-		egueb_dom_node_sibling_next_get(child, &sibling);
-		_myelement_dump(child, deep, level + 1);
-		child = sibling;
-	} while (child);
-done:
-	egueb_dom_node_unref(thiz);
-}
-#endif
-
 static void _element_name_over_cb(Egueb_Dom_Event *e, void *data)
 {
 	Egueb_Dom_Node *target = NULL;
@@ -127,6 +31,69 @@ static void _element_name_out_cb(Egueb_Dom_Event *e, void *data)
 	egueb_svg_color_components_from(&color, 0x00, 0x00, 0x00);
 	egueb_svg_element_color_set(target, &color);
 	egueb_dom_node_unref(target);
+}
+
+/* FIXME we should enable this once we have the <tspan> implemented */
+static void _attrs_create(Trantor_View_Xml *thiz, Egueb_Dom_Node *n)
+{
+#if 0
+	Egueb_Dom_Node_Map_Named *attrs = NULL;
+	int count;
+	int i;
+
+	egueb_dom_node_attributes_get(n, &attrs);
+	if (!attrs) return;
+
+
+	count = egueb_dom_node_map_named_length(attrs);
+	for (i = 0; i < count; i++)
+	{
+		Egueb_Dom_Node *attr = NULL;
+		Egueb_Dom_String *attr_name;
+		Egueb_Dom_String *attr_value = NULL;
+		Egueb_Dom_Node *text;
+		Egueb_Dom_Node *tnode;
+		Egueb_Svg_Length x, y;
+		Egueb_Svg_Font_Size font_size;
+
+		egueb_dom_node_map_named_at(attrs, i, &attr);
+		if (!attr) continue;
+		if (!egueb_dom_attr_is_set(attr)) goto no_name;
+
+		egueb_dom_attr_name_get(attr, &attr_name);
+		if (!attr_name) goto no_name;
+
+		egueb_dom_attr_string_get(attr, &attr_value);
+		if (!attr_value)
+		{
+			egueb_dom_string_unref(attr_name);
+			goto no_name;
+		}
+
+		text = egueb_svg_element_text_new();
+		font_size.type = EGUEB_SVG_FONT_SIZE_TYPE_LENGTH;
+		egueb_svg_length_set(&font_size.value.length, 12, EGUEB_SVG_UNIT_LENGTH_PX);
+		egueb_svg_element_font_size_set(text, &font_size);
+
+		egueb_svg_length_set(&y, 12 * thiz->level, EGUEB_SVG_UNIT_LENGTH_PX);
+		egueb_svg_element_text_y_set(text, &y);
+		egueb_svg_length_set(&x, 12 * thiz->tab, EGUEB_SVG_UNIT_LENGTH_PX);
+		egueb_svg_element_text_x_set(text, &x);
+
+		tnode = egueb_dom_text_new();
+		egueb_dom_character_data_append_data(tnode, attr_name);
+		egueb_dom_character_data_append_data_inline(tnode, "=\"");
+		egueb_dom_character_data_append_data(tnode, attr_value);
+		egueb_dom_character_data_append_data_inline(tnode, "\"");
+
+		egueb_dom_node_child_append(text, tnode);
+		egueb_dom_node_child_append(thiz->svg, text);
+no_name:
+		egueb_dom_node_unref(attr);
+	}
+
+	egueb_dom_node_map_named_free(attrs);
+#endif
 }
 
 static void _tag_create(Trantor_View_Xml *thiz, Egueb_Dom_Node *n, Eina_Bool open)
@@ -170,7 +137,9 @@ static void _tag_create(Trantor_View_Xml *thiz, Egueb_Dom_Node *n, Eina_Bool ope
 				_element_name_out_cb, EINA_TRUE, NULL);
 
 		egueb_dom_node_child_append(thiz->svg, text);
+
 		/* now the attributes */
+		_attrs_create(thiz, n);
 		/* now the children */
 		egueb_dom_node_child_first_get(n, &child);
 		if (child)
