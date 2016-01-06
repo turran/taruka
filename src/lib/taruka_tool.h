d42f2aeb6d96958ce06f5db33d73577780993944
Jorge Zapata <jorgeluis.zapata@gmail.com>
2016-01-07 00:53:00 +0100

    Add the tool interface

---
+++ src/lib/taruka_tool.h
@@ -0,0 +1,27 @@
+/* TARUKA - Egueb Editor
+ * Copyright (C) 2016 Jorge Luis Zapata
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef TARUKA_TOOL_H
+#define TARUKA_TOOL_H
+
+/* Interface for tools */
+typedef struct _Taruka_Tool_Descriptor
+{
+
+} Taruka_Tool_Descriptor;
+
+#endif
