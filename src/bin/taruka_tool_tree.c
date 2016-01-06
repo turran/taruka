14516b42d3cf6f65e106095daaca1ddce46b5d22
Jorge Zapata <jorgeluis.zapata@gmail.com>
2016-01-06 21:22:50 +0100

    Add the boilerplate for the tree tool
    We need to start defining the tool interface

---
+++ src/bin/taruka_tool_tree.c
@@ -0,0 +1,35 @@
+/* TARUKA - Egueb Editor
+ * Copyright (C) 2016 Jorge Luis Zapata
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "taruka_private.h"
+/*
+ * When need callbacks when:
+ * - A tool is loaded
+ * - A tool is going to be drawn (load the eon, import, etc, etc)
+ */
+/*============================================================================*
+ *                                  Local                                     *
+ *============================================================================*/
+/*----------------------------------------------------------------------------*
+ *                              Tool interface                                *
+ *----------------------------------------------------------------------------*/
+/*============================================================================*
+ *                                 Global                                     *
+ *============================================================================*/
+/*============================================================================*
+ *                                   API                                      *
+ *============================================================================*/
